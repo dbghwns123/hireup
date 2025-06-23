23382b05b25d728ea3c435bfc85815ced553bac5
Ho-JunYu <hojoon1011@gmail.com>
2025-06-23 18:57:14 +0900

    feat:일기 작성 요청 DTO(DiaryRequestDto) 추가

---
+++ src/main/java/com/project/hireup/dto/DiaryRequestDto.java
@@ -0,0 +1,13 @@
+package com.project.hireup.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryRequestDto {
+
+  private String Content;
+}
