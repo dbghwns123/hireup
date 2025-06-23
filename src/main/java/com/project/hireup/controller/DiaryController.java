36f942d2958a389fd7bd48ef5508d1c610ad3d73
Ho-JunYu <hojoon1011@gmail.com>
2025-06-23 19:30:04 +0900

    feat: 다이어리 생성 API(DiaryController) 구현

---
+++ src/main/java/com/project/hireup/controller/DiaryController.java
@@ -0,0 +1,25 @@
+package com.project.hireup.controller;
+
+import com.project.hireup.dto.DiaryRequestDto;
+import com.project.hireup.security.UserDetailsImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/diaries")
+public class DiaryController {
+
+  private final DiaryService diaryService;
+
+  @PostMapping
+  public ResponseEntity<Void> createDiary(@AuthenticationPrincipal UserDetailsImpl userDetails,
+      @RequestBody DiaryRequestDto diaryRequestDto) {
+    return ResponseEntity.ok(diaryService.createDiary(userDetails.getId(), diaryRequestDto));
+  }
+}
